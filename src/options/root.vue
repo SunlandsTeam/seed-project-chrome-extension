c67c5cfe01cd1571701a39d697121f7c89d70381
Donny <1021001390@qq.com>
2018-06-02 13:31:22 +0800

    Init commit

---
+++ src/options/root.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    div options
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style>
+div {
+  color: blue;
+}
+</style>
